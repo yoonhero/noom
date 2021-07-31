3cb2b825e7d5d25690b6f75694f6b745b1fc3f34
yoonhero <yoonhero06@naver.com>
2021-08-01 08:39:32 +0900

    #1.7 Nicknames part One

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,28 +1,40 @@
-const messageUl = document.querySelector("ul")
-const messageForm = document.querySelector("form")
+const messageList = document.querySelector("ul")
+const messageForm = document.querySelector("#message")
+const nickForm = document.querySelector("#nick")
 const socket = new WebSocket(`ws://${window.location.host}`)
+
+function makeMessage(type, payload) {
+  const msg = { type, payload }
+  return JSON.stringify(msg)
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server");
 })
 
 socket.addEventListener("message", (message) => {
-  console.log("Just got this", message.data, "from the server")
+  const li = document.createElement("li")
+  li.innerText = message.data
+  messageList.append(li)
 })
 
 socket.addEventListener("close", () => {
   console.log("DisConnected from Server X")
 })
 
-setTimeout(() => {
-  socket.send("hello from the browser")
-}, 1000)
-
 function handleSubmit(event) {
   event.preventDefault()
   const input = messageForm.querySelector("input")
-  socket.send(input.value)
+  socket.send(makeMessage("new_message", input.value))
   input.value = ""
 }
 
+function handleNickSubmit(event) {
+  event.preventDefault()
+  const input = nickForm.querySelector("input")
+  socket.send(makeMessage("nickname", input.value))
+
+}
+
 messageForm.addEventListener("submit", handleSubmit)
+nickForm.addEventListener("submit", handleNickSubmit)
