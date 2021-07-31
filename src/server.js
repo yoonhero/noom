1863befbe9dda3b2c2e3a5c02a5c87ce69cedeaa
yoonhero <yoonhero06@naver.com>
2021-08-01 08:49:37 +0900

    #1.8 NickNames part Two

--- src/server.js
+++ src/server.js
@@ -19,12 +19,20 @@ const sockets = []
 
 wss.on("connection", (socket) => {
   sockets.push(socket)
+  socket["nickname"] = ""
   console.log("Connected to Browser")
   socket.on("close", () => {
     console.log("DisConnected from the Browser")
   })
-  socket.on("message", (message) => {
-    sockets.forEach(aSocket => aSocket.send(message))
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg)
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`))
+
+      case "nickname":
+        socket["nickname"] = message.payload
+    }
   })
 
 })
