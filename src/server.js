a12389e22fe4b239bd473d02aa70072ed7902550
yoonhero <yoonhero06@naver.com>
2021-08-01 08:28:57 +0900

    #1.6 Chat Completed

--- src/server.js
+++ src/server.js
@@ -15,15 +15,18 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = []
+
 wss.on("connection", (socket) => {
+  sockets.push(socket)
   console.log("Connected to Browser")
   socket.on("close", () => {
     console.log("DisConnected from the Browser")
   })
   socket.on("message", (message) => {
-    console.log(message)
+    sockets.forEach(aSocket => aSocket.send(message))
   })
-  socket.send("hello!!!?");
+
 })
 
 server.listen(3000, handleListen);
