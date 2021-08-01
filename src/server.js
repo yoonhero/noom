07d8180cc8d0bf28e1c6cb811bbf66b4e990b953
yoonhero <yoonhero06@naver.com>
2021-08-01 21:07:10 +0900

    #2.4 Rooms

--- src/server.js
+++ src/server.js
@@ -14,14 +14,16 @@ app.get("/*", (_, res) => res.redirect("/"))
 
 const httpServer = http.createServer(app);
 // const wss = new WebSocket.Server({ httpServer });
+
 const wsServer = SocketIO(httpServer)
 
 wsServer.on("connection", socket => {
-  socket.on("enter_room", (msg, done) => {
-    console.log(msg)
-    setTimeout(() => {
-      done()
-    },1000)
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`)
+  })
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName)
+    done()
   });
 })
 
