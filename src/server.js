c9d2777e226bc2f52841fdd37760a39ad8afc526
yoonhero <yoonhero06@naver.com>
2021-08-01 22:38:00 +0900

    #2.11 Admin Panel

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http"
-import SocketIO from "socket.io"
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 import express from "express";
 
 const app = express();
@@ -15,7 +16,16 @@ app.get("/*", (_, res) => res.redirect("/"))
 const httpServer = http.createServer(app);
 // const wss = new WebSocket.Server({ httpServer });
 
-const wsServer = SocketIO(httpServer)
+const wsServer = new Server(httpServer, {
+   cors: {
+     origin: ["https://admin.socket.io"],
+     credentials: true,
+   },
+ });
+
+ instrument(wsServer, {
+   auth: false,
+ });
 
 function publicRooms() {
   const {sockets: {adapter: {sids, rooms}}} = wsServer
