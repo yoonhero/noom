649385f56ce958ed58062084303655f53b626b90
yoonhero <yoonhero06@naver.com>
2021-08-02 12:56:39 +0900

    course finished

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,6 +13,7 @@ let muted = false;
 let cameraOff = true
 let roomName
 let myPeerConnection
+let myDataChannel
 
 async function getCameras() {
 	try {
@@ -121,7 +122,15 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit)
 
 // Socket Code 
 
+//mydatachannel.send("")
+
 socket.on("welcome", async () => {
+	myDataChannel = myPeerConnection.createDataChannel("chat")
+	myDataChannel.addEventListener("message", (event)=>{
+		console.log(event.data)
+	})
+
+
 	const offer = await myPeerConnection.createOffer();
 	myPeerConnection.setLocalDescription(offer)
 	console.log("sent the offer") 
@@ -129,6 +138,13 @@ socket.on("welcome", async () => {
 })
 
 socket.on("offer", async (offer) => {
+	myPeerConnection.addEventListener("datachannel", (event)=> {
+		myDataChannel = event.channel
+		myDataChannel.addEventListener("message", (event)=>{
+			console.log(event.data)
+		})
+	})
+
 	console.log("received the offer") 
 	myPeerConnection.setRemoteDescription(offer)
   	const answer = await myPeerConnection.createAnswer()
