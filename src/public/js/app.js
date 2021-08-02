188936d68302ba73fcc77451c8f85d6689a622fe
yoonhero <yoonhero06@naver.com>
2021-08-02 10:28:26 +0900

    #3.5 Offers

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -12,6 +12,7 @@ let myStream;
 let muted = false;
 let cameraOff = true
 let roomName
+let myPeerConnection
 
 async function getCameras() {
 	try {
@@ -96,10 +97,11 @@ camerasSelect.addEventListener("input", handleCameraChange)
 const welcome = document.getElementById("welcome")
 welcomeForm = welcome.querySelector("form")
 
-function startMedia(){
+async function startMedia(){
 	welcome.hidden = true
 	call.hidden = false 
-	getMedia()
+	await getMedia()
+	makeConnection();
 }
 
 function handleWelcomeSubmit(event) {
@@ -114,8 +116,21 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit)
 
 // Socket Code 
 
-socket.on("welcome", () => {
-	console.log("someone joined")
+socket.on("welcome", async () => {
+	const offer = await myPeerConnection.createOffer();
+	myPeerConnection.setLocalDescription(offer)
+	socket.emit("offer", offer, roomName)
 })
+
+socket.on("offer", (offer) => {
+	console.log(offer)
+})
+
+// RTC Code 
+
+function makeConnection() {
+	myPeerConnection = new RTCPeerConnection()
+	myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream))
+}
 
 
--- src/server.js
+++ src/server.js
@@ -23,6 +23,9 @@ wsServer.on("connection", (socket) => {
     done()
     socket.to(roomName).emit("welcome");
   })
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer)
+  })
 })
 
 
